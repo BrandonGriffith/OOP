7bb38f7696d1b0800bbc7b41e5d23c1208cc3def
BrandonGriffith <BrandonGriffith92@gmail.com>
2024-01-08 11:13:09 -0500

    update

--- js_gran.mjs
+++ js_gran.mjs
@@ -4,7 +4,7 @@ import { execSync } from 'child_process'; // Import the execSync function from t
 class GetRandomFile {
     constructor() {
         this.rootdir = process.cwd(); // Get the current working directory
-        this.all_files = []; // Initialize an empty array to store all file paths
+        this.all_file_paths = []; // Initialize an empty array to store all file paths
     }
 
     recursive_scan_directory(dirname) {
@@ -15,21 +15,21 @@ class GetRandomFile {
         for (const directory_content of directory_contents) {
             const filePath = dirname + '\\' + directory_content.name; // Create the full path of the directory\file
             if (!directory_content.isDirectory()) {
-                this.all_files.push(filePath); // Add the file path to the array
+                this.all_file_paths.push(filePath); // Add the file path to the array
             } else {
-                this.all_files.concat(this.recursive_scan_directory(filePath)); // Recursively scan the subdirectory
+                this.all_file_paths.concat(this.recursive_scan_directory(filePath)); // Recursively scan the subdirectory
             }
         }
-        return; // Return the array of file paths
+        return;
     }
 
     start_pro() {
         try {
             this.recursive_scan_directory(this.rootdir); // Get all file paths in the project directory
-            if (this.all_files.length > 0) {
-                const randomIndex = Math.floor(Math.random() * (this.all_files.length + 1)); // Generate a random index
-                const randomFile = this.all_files[randomIndex]; // Get a random file path
-                console.log(randomFile); // Log the file
+            if (this.all_file_paths.length > 0) {
+                const randomIndex = Math.floor(Math.random() * (this.all_file_paths.length + 1)); // Generate a random index
+                const randomFile = this.all_file_paths[randomIndex]; // Get a random file path
+                console.log(randomFile); // Log the file path
                 const command = `"${randomFile.replace(/"'/g, '')}"`; // Create the command to execute
                 execSync(command, { timeout: 1000, shell: true }); // Execute the command
             } else {
