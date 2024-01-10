38e11421ca4668b3670f4353427b0f4076498e4a
BrandonGriffith <BrandonGriffith92@gmail.com>
2024-01-09 19:28:48 -0500

    update

---
+++ fibAlgo.js
@@ -0,0 +1,15 @@
+class Fibonacci {
+    constructor(n) {
+        this.n = n;
+    }
+    calculate() {
+        if (this.n <= 1) return console.log(this.n);
+        let fib = [0, 1];
+        for (let i = 2; i <= this.n; i++) {
+            fib[i] = fib[i - 1] + fib[i - 2];
+        }
+        return console.log(fib[this.n]);
+    }
+}
+
+new Fibonacci(10).calculate();
